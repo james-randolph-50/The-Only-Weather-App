811af93a9c57abe4a8d0ca36c6b5b1677db1f72d
James <james.randolph2016@gmail.com>
2019-03-06 18:57:57 -0500

    daily forecast changed

--- src/components/DailyForecast.js
+++ src/components/DailyForecast.js
@@ -2,11 +2,18 @@ import React from 'react';
 import moment from 'moment';
 
 const DailyForecast =({ forecastData }) => {
-    const renderForecasts = forecastData.map(({ precipIntensity, precipProbability, time }, index) => 
+    const renderForecasts = forecastData.map(({ apparentTemperatureMax, apparentTemperatureMin, precipProbability, humidity, summary, temperatureMax, temperatureMin, sunriseTime, sunsetTime, time }, index) => 
     <div key={index} style={{ border: 'solid 1px blue', padding: '10px', margin: '16px' }}>
         <p>{moment.unix(time).format('MMMM Do YYYY, h:mm a')}</p>
-        <p>Intensity of Percipitation: {precipIntensity}%</p>
+        <h4>{summary}</h4>
+        <p>High: {temperatureMax}</p>
+        <p>Low: {temperatureMin}</p>
+        <p>Feels Like High: {apparentTemperatureMax}</p>
+        <p>Feels Like Low: {apparentTemperatureMin}</p>
+        <p>Humidity: {humidity}%</p>
         <p>Chance of Percipitation: {precipProbability}%</p>
+        <p>Sunrise: {moment.unix(sunriseTime).format('h:mm a')}</p>
+        <p>Sunset: {moment.unix(sunsetTime).format('h:mm a')}</p>
 
     </div>
     )
