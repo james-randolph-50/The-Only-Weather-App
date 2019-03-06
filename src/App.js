e01bc996629ce494cb8888a06ed7a488446f01cc
James <james.randolph2016@gmail.com>
2019-03-06 15:10:04 -0500

    add apiurl, removed app.test.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const APIURL = `https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY_KEY}`
+
 class App extends Component {
   constructor() {
     super()
@@ -12,11 +14,18 @@ class App extends Component {
       }
   }
   render() {
+    const { fetchingData } = this.state
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className="App-header"></p>
+          <p className="App-header">
+            {
+              fetchingData ?
+              <img src={logo} className="App-logo" alt="logo" />
+              :
+              <h1>Data is received</h1>
+            }
+          </p>
         </header>
       </div>
     );
