646f871bcb06547312ccc69f0e9887f56d68e3f0
James <james.randolph2016@gmail.com>
2019-03-07 09:11:25 -0500

    building store, calling soon to be reducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,18 @@
+import { createStore, applyMiddleware } from 'reux';
+import thunk from 'redux-thunk';
+import fetchingData from '../reducers/fetchingDataReducer';
+import weatherData from '../reducers/weatherDataReducer';
+import route from '../reducers/routeReducer';
+
+const middlewares = [thunk];
+
+
+export default createStore(
+    fetchingData,
+    weatherData,
+    route,
+    applyMiddleware(...middlewares)
+)
 
 store = {
         fetchingData: true,
